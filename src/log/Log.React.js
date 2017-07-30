4d5abfb616dd26e6db5730fe2cd974d817904ae0
Lang <silentbalanceyh@126.com>
2017-07-30 13:27:00 +0800

    日志器处理完成

--- src/log/Log.React.js
+++ src/log/Log.React.js
@@ -1,8 +1,8 @@
-const _colorful = (reference = {}, Name, color = {}) => {
-    let message = `%c [Joy] [React Component] Control monitor: name = ${Name}`;
+const _colorful = (reference = {}, Name, color = {}, stateless) => {
+    let message = `%c [Joy] [${stateless?"React Component":"Stateless Function Component"}] Control monitor: name = ${Name}`;
     console.groupCollapsed(message, `color:${color.group};font-weight:900`);
     console.log(`%c [Joy] Props -> `, `color:${color.props};font-weight:900`, reference.props);
-    console.log(`%c [Joy] State -> `, `color:${color.state};font-weight:900`, reference.state);
+    if(!stateless) console.log(`%c [Joy] State -> `, `color:${color.state};font-weight:900`, reference.state);
     console.log(`%c [Joy] Hoc -> `, `color:${color.props};font-weight:900`, reference.hoc);
     console.groupEnd()
 };
@@ -15,12 +15,20 @@ const control = (reference = {}, Name) => {
     });
 };
 
-const stateless = (props = {}, Name, hoc = {}) => {
-    let message = `%c [Joy] [Stateless Function Component] Control monitor: name = ${Name}`;
-    console.groupCollapsed(message, `color:#99CC33;font-weight:900`);
-    console.log(`%c [Joy] Props -> `, `color:#333366;font-weight:900`, props);
-    console.log(`%c [Joy] Hoc -> `, `color:#3366CC;font-weight:900`, hoc);
-    console.groupEnd()
+const hoc = (reference = {}, Name) => {
+    _colorful(reference, Name, {
+        group: '#CC0066',
+        props: '#333366',
+        state: '#666666'
+    });
+};
+
+const stateless = (props = {}, Name) => {
+    _colorful({props,state:null}, Name, {
+        group: '#99CC33',
+        props: "#333366",
+        state: "#666666"
+    },true)
 };
 
 const container = (reference = {}, Name) => {
@@ -52,5 +60,6 @@ export default {
     container,
     components,
     form,
-    stateless
+    stateless,
+    hoc
 }
--- src/log/Log.js
+++ src/log/Log.js
@@ -2,9 +2,11 @@ import Ajax from './Log.Ajax'
 import React from './Log.React'
 export default {
     sign:Ajax.sign,
+    // React组件专用属性日志器
     control:React.control,
     container:React.container,
     components:React.components,
     form:React.form,
-    stateless:React.stateless
+    stateless:React.stateless,
+    hoc:React.hoc
 }
