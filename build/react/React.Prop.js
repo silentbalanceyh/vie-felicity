de46a3d5b484282c5148e72a691653e5a670647f
Lang <silentbalanceyh@126.com>
2017-09-09 23:32:11 +0800

    tabular/assist extraction

--- build/react/React.Prop.js
+++ build/react/React.Prop.js
@@ -17,6 +17,31 @@ var extract = function extract() {
     return attrs;
 };
 
+var _prefix = function _prefix() {
+    var props = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+    var keys = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : [];
+    var prefix = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : "";
+
+    var attrs = {};
+    keys.forEach(function (key) {
+        var dataKey = prefix + "_" + key.replace(/\./g, "_");
+        if (props.hasOwnProperty(dataKey)) {
+            attrs[dataKey] = props[dataKey];
+        }
+    });
+    return attrs;
+};
+
 exports.default = {
-    extract: extract
+    extract: extract,
+    t: function t() {
+        var props = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+        var keys = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : [];
+        return _prefix(props, keys, "t");
+    },
+    a: function a() {
+        var props = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+        var keys = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : [];
+        return _prefix(props, keys, "a");
+    }
 };
