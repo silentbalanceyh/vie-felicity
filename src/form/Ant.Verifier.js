d33f725b343c53e806d08232caf52334c3870509
huarui <evgreen@126.com>
2017-09-16 22:07:09 +0800

    multi fields validation

--- src/form/Ant.Verifier.js
+++ src/form/Ant.Verifier.js
@@ -25,11 +25,37 @@ const same = (form = {}) => (rule = {}, values, callback) => {
         return compared !== values ? rule.message : false;
     });
 };
+const lessEqual = (form = {}) => (rule = {}, values, callback) => {
+    // 1.提取target字段
+    if (!rule["$comparedTo"]) {
+        console.warn(
+            "[JOY] Please check your configuration for rule: lessEqual. '$comparedTo' missing."
+        );
+    }
+    // 2.前置条件判断
+    Cond.execute(form, rule, callback, () => {
+        const compared = form.getFieldValue(rule.$comparedTo);
+        return compared < values ? rule.message : false;
+    });
+};
+const moreEqual = (form = {}) => (rule = {}, values, callback) => {
+    // 1.提取target字段
+    if (!rule["$comparedTo"]) {
+        console.warn(
+            "[JOY] Please check your configuration for rule: moreEqual. '$comparedTo' missing."
+        );
+    }
+    // 2.前置条件判断
+    Cond.execute(form, rule, callback, () => {
+        const compared = form.getFieldValue(rule.$comparedTo);
+        return compared > values ? rule.message : false;
+    });
+};
 const after = (form = {}) => (rule = {}, values, callback) => {
     // 1.提取target字段
     if (!rule["$comparedTo"]) {
         console.warn(
-            "[JOY] Please check your configuration for rule: same. '$comparedTo' missing."
+            "[JOY] Please check your configuration for rule: after. '$comparedTo' missing."
         );
     }
     // 2.前置条件判断
@@ -48,7 +74,7 @@ const before = (form = {}) => (rule = {}, values, callback) => {
     // 1.提取target字段
     if (!rule["$comparedTo"]) {
         console.warn(
-            "[JOY] Please check your configuration for rule: same. '$comparedTo' missing."
+            "[JOY] Please check your configuration for rule: before. '$comparedTo' missing."
         );
     }
     // 2.前置条件判断
@@ -97,10 +123,33 @@ const duplicated = (form = {}, generator = {}) => (
     if (ready) {
         if (values) {
             const uri = rule.uri ? rule.uri : "/api/vlv/rule/duplicated";
-            const { identifier, name } = rule.$ajax;
-            const params = { identifier, name, value: values };
+            const { identifier, name, uniqueId = "uniqueId" } = rule.$ajax;
+            let targetValues = [];
+            let targetFields = [];
+            targetFields.push(name);
+            targetValues.push(values);
             // 附加参数
-            _prepare(form, rule, params);
+            if (rule["$params"]) {
+                for (const name in rule.$params) {
+                    const target = rule.$params[name];
+                    if (target) {
+                        const value = form.getFieldValue(target);
+                        if (value) {
+                            targetFields.push(target);
+                            targetValues.push(value);
+                        }
+                    }
+                }
+            }
+            const params = {
+                identifier,
+                name: targetFields,
+                value: targetValues
+            };
+            // 更新流程中，重新设定了unquieId则使用重新设定后的unquieId，否则取默认的unquieId，添加流程中什么也不取
+            if (form.getFieldValue(uniqueId)) {
+                params["uniqueId"] = form.getFieldValue(uniqueId);
+            }
             const promise = generator.post(uri, params);
             promise.then(data => {
                 if (data) {
@@ -155,6 +204,8 @@ const existing = (form = {}, generator = {}) => (
 export default {
     diff,
     same,
+    lessEqual,
+    moreEqual,
     required,
     duplicated,
     existing,
